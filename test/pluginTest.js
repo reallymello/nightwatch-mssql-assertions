8df276e0175e8b15d2ddc83d1ce0bb6bdaf43eff
David Mello <theycallmedavidmello@gmail.com>
2023-01-16 20:48:54 -0500

    Test

--- test/pluginTest.js
+++ test/pluginTest.js
@@ -1,9 +1,21 @@
 module.exports = {
-  test: () => {
-    browser.assert.recordCountIs(
+  test: async () => {
+    const sql = require('mssql');
+
+    try {
+      await sql.connect(
+        'Server=localhost,1433;Database=database;User Id=sa;Password=ThisIsAStrongP@assword!SortOf;Encrypt=true'
+      );
+      const result = await sql.query`select * from mytable`;
+      console.dir(result);
+    } catch (err) {
+      console.error(err);
+    }
+
+    /*browser.assert.recordCountIs(
       1,
       'people',
       "first_name = 'Jane' AND last_name = 'Doe'"
-    );
+    );*/
   },
 };
