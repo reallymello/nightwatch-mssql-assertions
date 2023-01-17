009213f34989a9d2c0f9b8030bb27db8df59b56d
David Mello <theycallmedavidmello@gmail.com>
2023-01-16 21:08:35 -0500

    Create table

--- test/pluginTest.js
+++ test/pluginTest.js
@@ -16,16 +16,38 @@ module.exports = {
     console.log(config);
     try {
       await sql.connect(config);
-      const result = await sql.query`SELECT GETDATE();`;
+      //const result = await sql.query`SELECT GETDATE();`;
+      const result = await sql.query(
+        `CREATE DATABASE nightwatchDb;
+
+        GO
+        USE nightwatchDb
+        CREATE TABLE people(
+            person_id INT PRIMARY KEY IDENTITY,
+            first_name VARCHAR(200),
+            last_name VARCHAR(200)
+        );
+        
+        GO
+        
+        INSERT INTO 
+            dbo.people(first_name, last_name)
+        VALUES
+            ('John', 'Doe'),
+            ('Jane', 'Doe'),
+            ('Really', 'Mello');
+            
+        GO`
+      );
       console.dir(result);
     } catch (err) {
       console.error(err);
     }
 
-    /*browser.assert.recordCountIs(
+    browser.assert.recordCountIs(
       1,
       'people',
       "first_name = 'Jane' AND last_name = 'Doe'"
-    );*/
+    );
   },
 };
